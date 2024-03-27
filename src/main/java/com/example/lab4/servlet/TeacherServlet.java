f2cb978913d398c33b0835b800e35e1b21da6d81
Andrew Grom <SagittariusDew@gmail.com>
2024-03-27 10:31:28 +0200

    mark builder

--- src/main/java/com/example/lab4/servlet/TeacherServlet.java
+++ src/main/java/com/example/lab4/servlet/TeacherServlet.java
@@ -1,21 +1,17 @@
 package com.example.lab4.servlet;
 
 import com.example.lab4.entity.Mark;
-import com.example.lab4.entity.Student;
 import com.example.lab4.repository.MarkRepository;
 import com.example.lab4.repository.StudentRepository;
 import com.example.lab4.repository.SubjectRepository;
 import com.example.lab4.utils.XssProtection;
 import jakarta.ejb.EJB;
-import jakarta.servlet.RequestDispatcher;
-import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
-import java.util.List;
 
 @WebServlet(name = "teacherServlet", value = "/teacher")
 public class TeacherServlet extends HttpServlet {
@@ -37,23 +33,15 @@ public class TeacherServlet extends HttpServlet {
             XssProtection.displayErrorPage(resp);
             return;
         }
-        Integer mark = Integer.valueOf(req.getParameter("mark"));
+        Integer point = Integer.valueOf(req.getParameter("mark"));
         Long studentId = Long.valueOf(req.getParameter("studentId"));
         Long subjectId = Long.valueOf(req.getParameter("subjectId"));
         Boolean isPresent = req.getParameter("isPresent") != null;
-        Mark newMark = getNewMark(mark, studentId, subjectId, isPresent);
-        markRepository.save(newMark);
+        markRepository.save(Mark.builder()
+                .point(point)
+                .student(studentRepository.findById(studentId))
+                .subject(subjectRepository.findById(subjectId))
+                .presence(isPresent)
+                .build());
     }
-
-    private Mark getNewMark(Integer mark, Long studentId, Long subjectId, Boolean isPresent) {
-        Mark newMark = new Mark();
-
-        newMark.setPoint(mark);
-        newMark.setStudent(studentRepository.findById(studentId));
-        newMark.setSubject(subjectRepository.findById(subjectId));
-        newMark.setPresence(isPresent);
-
-        return newMark;
-    }
-
 }
